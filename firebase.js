2a57dc7ab2c6dbd7b2bf2cac7a1d27bbd7f55a93
desartstudio95 <desartstudiopro@gmail.com>
2026-05-16 10:03:11 -0700

    Refactor Firebase initialization and export structure

--- firebase.js
+++ firebase.js
@@ -1,13 +1,24 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_KEY
-);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential:
+    admin.credential.cert({
+      projectId:
+        process.env.FIREBASE_PROJECT_ID,
+
+      clientEmail:
+        process.env.FIREBASE_CLIENT_EMAIL,
+
+      privateKey:
+        process.env
+          .FIREBASE_PRIVATE_KEY
+          ?.replace(/\\n/g, '\n')
+    })
 });
 
 const db = admin.firestore();
 
-module.exports = db;
+module.exports = {
+  admin,
+  db
+};
