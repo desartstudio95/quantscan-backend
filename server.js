826b0fc014898df499fd098c976fb009ca9db0e0
desartstudio95 <desartstudiopro@gmail.com>
2026-05-16 07:26:16 -0700

    Implement /price/:pair endpoint for price retrieval
    Added a new endpoint to fetch price data for a given pair.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./firebase");
+const getPrice = require("./services/priceService");
 
 const app = express();
 
@@ -8,6 +10,29 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("QuantScan Backend Online 🚀");
+});
+
+app.get("/price/:pair", async (req, res) => {
+
+  try {
+
+    const pair = req.params.pair;
+
+    const price = await getPrice(pair);
+
+    res.json({
+      pair,
+      price
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 });
 
 app.listen(process.env.PORT || 3000, () => {
