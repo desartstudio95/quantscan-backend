b94a42af23a2aadbe7de62b3a635f6a01859478a
desartstudio95 <desartstudiopro@gmail.com>
2026-05-16 09:11:33 -0700

    Add multiTimeframeService for market analysis

---
+++ services/multiTimeframeService.js
@@ -0,0 +1,17 @@
+const analyzeMTF = () => {
+
+  return {
+
+    higherTimeframe: "BULLISH",
+
+    mediumTimeframe: "BULLISH",
+
+    lowerTimeframe: "BUY",
+
+    alignment: true
+
+  };
+
+};
+
+module.exports = analyzeMTF;
